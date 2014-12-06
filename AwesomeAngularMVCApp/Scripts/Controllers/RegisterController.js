0c044ec2e6a586a4abf26007b2377ac60c8dd7b9
AmadeoPadula <padula.amadeo@gmail.com>
2014-12-05 18:55:11 -0600

    Initial Commint

---
+++ AwesomeAngularMVCApp/Scripts/Controllers/RegisterController.js
@@ -0,0 +1,12 @@
+﻿var RegisterController = function($scope) {
+    $scope.registerForm = {
+        emailAddress: '',
+        password: '',
+        confirmPassword: ''
+    };
+    $scope.register = function() {
+        //todo
+    }
+}
+
+RegisterController.$inject = ['$scope'];
