67e45ae351402c3498965828f1f4bc8848f84856
AmadeoPadula <padula.amadeo@gmail.com>
2014-12-10 11:10:06 -0600

    End of part One

--- AwesomeAngularMVCApp/Scripts/AwesomeAngularMVCApp.js
+++ AwesomeAngularMVCApp/Scripts/AwesomeAngularMVCApp.js
@@ -2,16 +2,19 @@
 
 AwesomeAngularMVCApp.controller('LandingPageController', LandingPageController);
 AwesomeAngularMVCApp.controller('LoginController', LoginController);
-
+AwesomeAngularMVCApp.factory('LoginFactory', LoginFactory);
 AwesomeAngularMVCApp.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
 
-var configFunction = function ($routeProvider, $httpProvider) {
+var configFunction = function ($routeProvider, $httpProvider, $locationProvider) {
+
+    $locationProvider.hashPrefix('!').html5Mode(true);
+
     $routeProvider.
         when('/routeOne', {
             templateUrl: 'routesDemo/one'
         })
         .when('/routeTwo/:donuts', {
-            templateUrl: function(params) { return '/routesDemo/two?donuts=' + params.donuts; }
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
         })
         .when('/routeThree', {
             templateUrl: 'routesDemo/three'
@@ -27,6 +30,5 @@ var configFunction = function ($routeProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
 }
-configFunction.$inject = ['$routeProvider', '$httpProvider'];
-
+configFunction.$inject = ['$routeProvider', '$httpProvider', '$locationProvider'];
 AwesomeAngularMVCApp.config(configFunction);
--- AwesomeAngularMVCApp/Scripts/Factories/LoginFactory.js
+++ AwesomeAngularMVCApp/Scripts/Factories/LoginFactory.js
@@ -1,22 +1,26 @@
-﻿var LoginFactory = function($http, $q) {
-    return function(emailAddress, password, rememberMe) {
+﻿var LoginFactory = function ($http, $q) {
+    return function (emailAddress, password, rememberMe) {
+
         var deferredObject = $q.defer();
-        $http.postMessage(
+
+        $http.post(
             '/Account/Login', {
                 Email: emailAddress,
                 Password: password,
                 RememberMe: rememberMe
             }
-        ).success(function(data) {
+        ).
+        success(function (data) {
             if (data == "True") {
                 deferredObject.resolve({ success: true });
             } else {
                 deferredObject.resolve({ success: false });
             }
-
-        }).error(function() {
+        }).
+        error(function () {
             deferredObject.resolve({ success: false });
         });
+
         return deferredObject.promise;
     }
 }
